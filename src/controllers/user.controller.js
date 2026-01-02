ec27754ec6faebe3734575cc13f952f2bcdc5e03
ZinedineZidane-05 <subhadeepsahoo2003@gmail.com>
2026-01-02 23:14:27 +0530

    fix refresh token validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -173,7 +173,7 @@ return res.status(200).clearCookie("accessToken", options).clearCookie("refreshT
 const refreshAccessToken = asyncHandler(async (req,res) =>{
   req.cookies.refreshToken || req.body.refreshToken
 
-  if(incomingRefreshToken) {
+  if(!incomingRefreshToken) {
     throw new ApiError(401, "unauthorized request")
   }
 
